5e732cc48d65640f85c17e1d9b92d8697e0083ca
Selman GÖKTAŞ <selman@karatekin.edu.tr>
2020-12-20 12:48:10 +0300

    string controllers

--- CoreFirstApp/Startup.cs
+++ CoreFirstApp/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,28 +7,26 @@ namespace CoreFirstApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //servis ekleme iþlemleri
+
+            services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+
+            //hangi servisin hangi sýra ve ayar ile kullanýlacaðý
+
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+            else app.UseStatusCodePagesWithReExecute("/Error", "?code={0}");//www.abc.com/Error?code=404   501 429
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
