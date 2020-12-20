6d6ae3e986267f1d956ed4caca05211a3fec5291
Selman GÖKTAŞ <selman@karatekin.edu.tr>
2020-12-20 14:29:58 +0300

    view model eklendi

--- CoreFirstApp/Controllers/HomeController.cs
+++ CoreFirstApp/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreFirstApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreFirstApp.Controllers
@@ -10,12 +11,22 @@ namespace CoreFirstApp.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.ulke = "Türkiye";
-            string[] ulkeler = { "Türkiye", "Almanya", "Çin", "Rusya" };
-            ViewBag.ulkeler = ulkeler;
+            //ViewBag.ulke = "Türkiye";
+            //string[] ulkeler = { "Türkiye", "Almanya", "Çin", "Rusya" };
+            //ViewBag.ulkeler = ulkeler;
 
-            ViewData["ulkeler"] = ulkeler;
-            return View();
+            //ViewData["ulkeler"] = ulkeler;
+
+            //TempData["basvuru"] = "asdadjjjjeEAA";
+            ////return View();
+            ///
+            // return RedirectToAction("Detail");
+
+            //Product product = new Product { ID = 1, Name = "Klavye", Picture = "1.jpg", Price = 520 };
+            //Category category = new Category { ID = 1, Name = "Elektronik Kategorisi" };
+            //return View(product);
+
+
         }
 
         public IActionResult Detail()
@@ -25,6 +36,9 @@ namespace CoreFirstApp.Controllers
 
         //Controllerden View e veri taşıma
         //1.ViewBAg
+        //2 VewData
+        //3 Tempdata -- bir defeya mahsus veri almak için 
+
 
     }
 }
---
+++ CoreFirstApp/ViewModels/ProductCategoryVM.cs
@@ -0,0 +1,14 @@
+﻿using CoreFirstApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreFirstApp.ViewModels
+{
+    public class ProductCategoryVM
+    {
+        public IEnumerable<Product> products;
+        public IEnumerable<Category> categories;
+    }
+}
